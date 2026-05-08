a712404c276c6d590b657690a15ff6b862b16fbd
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-05-08 19:23:01 +0700

    feat: implement employee list component with axios service integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import HelloWorld from "./HelloWorld";
+import ListEmployeeComponent from "./component/ListEmployeeComponent";
 
 function App() {
   return (
     <>
-      <HelloWorld />
+      <ListEmployeeComponent />
     </>
   );
 }
