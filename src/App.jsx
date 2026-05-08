f4b7cb6f7a16e5c3de702a2661a03b5e06843f9f
thinhnguyenxuan <ngxthinh271@gmail.com>
2026-05-08 21:24:17 +0700

    feat: install react-router-dom and implement application layout with header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import ListEmployeeComponent from "./component/ListEmployeeComponent";
-
+import HeaderComponent from "./component/HeaderComponent";
+import FooterComponent from "./component/FooterComponent";
+import { BrowserRouter, Route, Routes }  from "react-router-dom";
 function App() {
   return (
     <>
-      <ListEmployeeComponent />
+    <BrowserRouter>
+      <HeaderComponent />
+        <Routes>
+        {/* // http://localhost:3000 */}
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          {/* // http://localhost:3000/employees */}
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>        
+      <FooterComponent />
+    </BrowserRouter>
     </>
   );
 }
